4fe245b18305825dfd73544e795f0a5d842f3706
Timo Sand <timo.j.sand@gmail.com>
2016-11-12 03:22:39 +0200

    Fixes wrong URL

--- 750words-archiver.user.js
+++ 750words-archiver.user.js
@@ -19,7 +19,7 @@
             if ( num_words >= 750 ) {
                 console.dir(event);
                 var request = new XMLHttpRequest();
-                request.open('POST', 'https://powerful-cove-69945.herokuapp.com/', true);
+                request.open('POST', 'https://powerful-cove-69945.herokuapp.com/750', true);
                 request.setRequestHeader('Content-Type', 'application/json');
                 request.send({"body": body, "words": num_words, "date": date });
             }
